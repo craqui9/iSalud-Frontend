bb0f0490721fe958c86ac857ce78792b2ac855f3
Daniel <daniru9@gmail.com>
2020-10-17 21:33:57 +0200

    Diseño del login (falta el icono y el titulo) (ahora el boton funciona)

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonInput } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
+  //Datos del formulario
+  @ViewChild('correoUsuario') correoUsuario: IonInput;
+  @ViewChild('contraseñaUsuario') contraseñaUsuario: IonInput;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  login(){
+    
+    
+    
   }
 
 }
